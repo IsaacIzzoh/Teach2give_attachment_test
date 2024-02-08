8dd750419d4d1e5ad1160d6993d5503acd178c00
IsaacIzzoh <124176530+IsaacIzzoh@users.noreply.github.com>
2024-02-08 13:00:19 +0300

    Update question_6.cpp

--- question_6.cpp
+++ question_6.cpp
@@ -1,23 +1,29 @@
+/*
+Question 6: Count Vowels
+Write a program that counts the number of vowels in a sentence.
+eg " Hello World " => returns 2
+ */
+
 #include <iostream> 
 #include <string> 
 
 using namespace std; 
 
 int main() { 
-    int count = 0; // I am declaring an integer variable called count and initialize it to 0
-    string sentence; // I am declaring a string variable called sentence to store the user's input
+    int count = 0; 
+    string sentence; 
 
-    cout << "Enter a sentence: "; // Output 
-    getline(cin, sentence); // Read a line of text from the user and store it in the sentence variable
+    cout << "Enter a sentence: "; 
+    getline(cin, sentence); 
 
-    for (char c : sentence) { // I am starting a for loop that iterates over each character in the sentence
+    for (char c : sentence) { 
         if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U') {
-            // If the character is a vowel (either lowercase or uppercase), increment the count
+            
             count++;
         }
     }
 
-    cout << "Number of vowels in the sentence: " << count << endl; // Output the count of vowels along with a message
+    cout << "Number of vowels in the sentence: " << count << endl; 
 
     return 0; 
 }
