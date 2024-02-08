a6175d22945cad5da5bff744a9eb5c2f2446ff96
IsaacIzzoh <124176530+IsaacIzzoh@users.noreply.github.com>
2024-02-08 13:02:57 +0300

    Update question_4.cpp

--- question_4.cpp
+++ question_4.cpp
@@ -1,3 +1,11 @@
+/*
+Question 4: Capitalize Words
+Write a program that accepts a string as input, capitalizes the first letter of each word in the 
+string, and then returns the result string.
+Examples: 
+"hi"=> returns "Hi"
+"i love programming"=> returns "I Love Programming"
+*/
 #include <iostream>
 #include <string>
 #include <cctype> 
